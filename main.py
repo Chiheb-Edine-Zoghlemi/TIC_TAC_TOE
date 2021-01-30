551c0de2ca02472dbf255cb23f13aa68d630547d
Chiheb <chihebxxx@gmail.com>
2021-01-30 21:22:14 +0100

    tic-tac_toe 2player modes only

---
+++ main.py
@@ -0,0 +1,195 @@
+from player import player
+import numpy as np 
+import sys
+from colorama import init
+init(strip=not sys.stdout.isatty()) 
+from termcolor import cprint 
+from pyfiglet import figlet_format
+
+
+def Winner_Exist(p1,p2):
+    if p1.status == True : 
+        cprint('\nPlayer  1  Wins \n','green')
+        return True 
+    elif p2.status == True : 
+        cprint('\n Player 2 Wins \n ','green')
+        return True
+    else : 
+        return False
+
+#for responsuve  display 
+def creat_line(dimension):
+    line='\n'
+    for i in range(dimension) : 
+        line  = line+'-------'
+    line=line+'\n'
+    return line
+
+#for displaying the matrix 
+def display_matrix(matrix,dimension) : 
+    line=creat_line(dimension)
+    ch=line
+    for i in  range(dimension):
+        for j  in range(dimension) : 
+            index=matrix[i][j]
+            if type(index) == int :
+                if index < 10 :
+                    ch=ch+("|  {index}  |".format(index=index))
+                elif index<100 : 
+                    ch=ch+("|  {index} |".format(index=index))
+                else : 
+                    ch=ch+("| {index} |".format(index=index))
+            else : 
+                ch=ch+("|  {index}  |".format(index=index))
+
+        ch=ch+line
+    print(ch)
+
+# display the copyright section when the program exist      
+def exit_program() : 
+    cprint('\n===============================================================','blue')
+    cprint('Developped By : Chiheb Edine Zoghlemi & Malek Hammou','red')
+    print('Contact Links -->   Chiheb Edine Zoghlemi')
+    print('Linkdin => https://www.linkedin.com/in/chihebedinezoghlemi/ ')
+    print('Github => https://github.com/Chiheb-Edine-Zoghlemi ')
+    print('Contact Links -->   Malek Hammou')
+    print('Linkdin => https://www.linkedin.com/in/malekhammou/ ')
+    print('Github => https://github.com/malekhammou ')
+    cprint('===============================================================','blue')
+    
+# this function interact with the user to create the matrix it returns matrix,dimension
+def set_matrix() : 
+    cprint('Please chose the matrix dimension \n','yellow')
+    while True : 
+        try : 
+            dimension=int(input())
+            if dimension > 0 : 
+                break 
+            else : 
+                cprint('Please chose the matrix dimension \n','yellow')
+        except : 
+            cprint('Please chose the matrix dimension \n','yellow')
+       
+    matrix = [ [ 0 for i in range(dimension) ] for j in range(dimension) ]
+    index=1
+    for i in range(dimension):
+        for j in range(dimension) : 
+            matrix[i][j]=index
+            index=index+1
+    display_matrix(matrix,dimension)
+    return dimension,matrix
+#set the player // return a player 
+def set_player(dimension,i) : 
+    p=player()
+    p.set_wining_moves(dimension)
+    while True : 
+        print('Player {i} Chose Your Symbole \n '.format(i=i))
+        symbole=input('\n')
+        try :
+            symbole=int(symbole)
+        except : 
+            p.set_choice(choice=symbole)
+            break 
+    return  p
+
+def menu () : 
+            cprint('\n===============================================================','blue')
+            cprint('                              Game Menu','red')
+            cprint('===============================================================','blue')
+            print('2 Player Mode              || Press 1 ')
+            print('Player VS Bot              || Press 2 ')
+            print('To change matrix dimension || Press 3 ')
+            print('To Exit                    || Press + ')
+            
+            
+            #verify the user choice
+            while True :
+                #get the user choice 
+                choice=input('\n')
+                try : 
+                    choice = int(choice)
+                    if choice == 1 or choice == 2  or choice == 3: 
+                        break
+                    else : 
+                        cprint('Please  provide a valid choice ','yellow')
+                except : 
+                    if  choice == '+' : 
+                        break 
+                    else :
+                        cprint('Please  provide a valid choice ','yellow')
+            return choice
+#the game 
+def game(p1,p2,matrix,dimension):
+    Reserved_Moves=[]
+    for role in range(dimension**2) :
+        if Winner_Exist(p1,p2): 
+            display_matrix(matrix,dimension)
+            break 
+        elif role % 2 == 0 : 
+            cprint('\nPLAYER 1 ','yellow')
+            display_matrix(matrix,dimension)
+            choice=p1.play(Reserved_Moves,dimension)
+            postion=[(index, row.index(choice)) for index, row in enumerate(matrix) if choice in row]
+            postion=postion[0]
+            matrix[postion[0]][postion[1]] = p1.choice
+            Reserved_Moves.append(choice)
+        else : 
+            cprint('\nPLAYER 2 ','yellow')
+            display_matrix(matrix,dimension)
+            choice=p2.play(Reserved_Moves,dimension)
+            postion=[(index, row.index(choice)) for index, row in enumerate(matrix) if choice in row]
+            postion=postion[0]
+            matrix[postion[0]][postion[1]] = p2.choice
+            Reserved_Moves.append(choice)
+    if role == dimension**2 : 
+        cprint('---- DRAW ----','yellow')
+#in case he chose the 2 player mode 
+def twoplayers(matrix,dimension):
+    cprint('\n===============================================================','blue')
+    cprint('                         2 PLAYERS MODE','red')
+    cprint('===============================================================','blue')
+    p1=set_player(dimension,1)
+    p2=set_player(dimension,2)
+    game(p1,p2,matrix,dimension)
+
+# in case he chose the vs bot mode 
+def vsbot(matrix,dimension):
+    cprint('\n===============================================================','blue')
+    cprint('                         VS BOT MODE','red')
+    cprint('===============================================================','blue')
+    display_matrix(matrix,dimension)
+
+#switch case implemented in python 
+def Switcher(i,matrix,dimension):
+        switcher={
+                1:twoplayers,
+                2:vsbot
+                }
+        func=switcher.get(i,lambda :'Invalid Choice')
+        func(matrix,dimension) 
+#main section 
+def main() : 
+    cprint(figlet_format('TIC', font='broadway'),'green', attrs=['bold'])
+    cprint(figlet_format('TAC', font='broadway'),'green', attrs=['bold'])
+    cprint(figlet_format('TOE', font='broadway'),'green', attrs=['bold'])
+    
+    cprint('                      Setting Board Dimension','red')
+    cprint('===============================================================\n','blue')
+    dimension,matrix=set_matrix()
+    
+    while True :
+        choice = menu()
+        if choice == '+' : 
+            exit_program()
+            break 
+        elif choice==3 : 
+            dimension,matrix=set_matrix()
+        else : 
+            Switcher(choice,matrix,dimension)
+
+
+
+
+
+if __name__ == '__main__' : 
+    main()
---
+++ player.py
@@ -0,0 +1,100 @@
+import sys
+from colorama import init
+init(strip=not sys.stdout.isatty())
+from termcolor import cprint 
+
+class player () : 
+    def __init__(self): 
+        self.moves=[] 
+        self.choice=''
+        self.status=False
+        wining_moves=[]
+    def set_choice(self,choice): 
+        self.choice=choice
+    def flatt_array(self):
+        flat=[]
+        for sublist in self.wining_moves:
+            for item in sublist:
+                flat.append(item)
+        return(flat)
+
+    def wins_by_colunm(self,dimension):
+        moves=[]
+        for i in range(1,dimension+1) :
+            combo=[]
+            for j in range(i,dimension**2+1,dimension) : 
+                combo.append(j)
+            moves.append(combo)
+        return  moves
+
+
+        
+    def wins_by_row(self,dimension):
+        moves=[]
+        for i in range(1,dimension**2+1,dimension) :
+            combo=[]
+            for j in range(i,dimension+i) : 
+                combo.append(j)
+            moves.append(combo)
+        return  moves
+
+        
+    def wins_by_diag(self,dimension):
+        moves=[]
+        combo=[]
+        for i in range(1,dimension**2+1 ,dimension+1) :
+            combo.append(i)
+        moves.append(combo)
+        
+        combo=[]
+        for i in range(dimension,(dimension**2+1)-(dimension-1) ,dimension-1) :
+            combo.append(i)
+        moves.append(combo)
+        return moves
+    
+    def set_wining_moves(self,dimension) : 
+        wining_moves_by_row=self.wins_by_row(dimension)
+        wining_moves_by_colunm=self.wins_by_colunm(dimension)
+        wining_moves_by_diag=self.wins_by_diag(dimension)
+        self.wining_moves=[wining_moves_by_row,wining_moves_by_colunm,wining_moves_by_diag]
+    
+    
+    def winner(self): 
+        my_moves = self.moves 
+        combos=self.flatt_array()
+        winner=False
+        for c in  combos :
+            if winner == True :
+                break 
+            else : 
+                winner = True 
+                for element in c : 
+                    if element not  in my_moves : 
+                        winner = False
+                        break  
+        if winner : 
+            self.status=True
+
+    def play(self,Reserved_Moves,dimension) : 
+        while True : 
+        # the user will provide I,J of   the move 
+            cprint('\nYour Turn Chose Position :)','yellow')
+            choice=input('\n') 
+            try : 
+                choice=int(choice)
+                # validate the I,J  
+                if  choice>0 and choice <=  dimension**2 :  
+                    if choice in Reserved_Moves : 
+                          cprint('\nPosition is taken','yellow')
+                    else : 
+                        break
+                else : 
+                    # MUST BE IN THE MATRIX  #0<i,j<dimension
+                    cprint('\nUnvalid Choice Please Chose  between {min} --- {max} \n'.format(min=1,max=dimension**2),'yellow')
+            except : 
+                cprint('Unvalid Choice Please Chose  between {min} --- {max} \n'.format(min=1,max=dimension**2),'yellow')
+        self.moves.append(choice)
+        if  len(self.moves) >=dimension:
+            self.winner()
+        return choice
+
